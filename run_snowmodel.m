4d920f15837eef01fa0cbff2f2999157cff70065
John <jabatzoglou@ucmerced.edu>
2021-03-08 11:49:33 -0800

    Update run_snowmodel.m

--- run_snowmodel.m
+++ run_snowmodel.m
@@ -1,32 +1,18 @@
-% if you want to apply CO2 resistance correction for elevated CO2
-% Kruijt, B., Witte, J.P.M., Jacobs, C.M. and Kroon, T., 2008. Effects of rising atmospheric CO2 on evapotranspiration and soil moisture: A practical approach for the Netherlands. Journal of Hydrology, 349(3-4), pp.257-267.
-if isvar(degrees)
-if degrees==2
- petdata=petdata*.978;
- dd='2';
-else
- petdata=petdata*.948;
- dd='4';
-end
-end
-
 % loop over years to load in tmax/tmin/pet/ppt data
 for yr=1:31
  tmean=tmaxdata/2+tmindata/2;
  clear tmaxdata tmindata dataout
  tmean=shiftdim(tmean,2);
  pptdata=shiftdim(pptdata,2);
- petdata=shiftdim(petdata,2);
 
 % f refers to non-empty cells if needed
 
  tmean=tmean(:,f);
  pptdata=pptdata(:,f);
- petdata=single(petdata(:,f));
 
 % carry forward snow from previous december or spinup if needed; otherwise start with some default snow
 
- [ROSNOW.data]=hydro_tax_ro(tmean',pptdata',petdata',snowlast');
+ [ROSNOW.data]=snowrun(tmean',pptdata',snowlast');
 snowlast=snowdata(12,:);
 
 snowdata=round(snowdata,1);
